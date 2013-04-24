7b2363020ddecbb213d23055ad5b3da06effb220
FErArg <ferarg@ferarg.com>
2013-04-24 08:24:10 +0200

    + ‘Bash/’ + ‘Bash/check_host_online_mysql.sh’ + ‘Bash/network.db’

---
+++ Bash/check_host_online_mysql.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DB="network.db"
+MySQLU="USER"
+MySQLP="PASSWORD"
+MySQLD="DATABASE"
+MySQLS="SERVER"
+MySQLT="TABLE"
+
+fecha="$(date +%F)"
+hora="$(date +%H.%M)"
+
+fecha2="$fecha $hora"
+echo $fecha2
+echo ""
+echo ""
+
+TOT=`wc -l < $DB`
+I=1
+while [ $I -le $TOT ]
+do
+ linea=`sed -n ${I}l $DB`
+ ip=`echo $linea | cut -d";" -f1`
+ host=`echo $linea | cut -d";" -f2`
+
+ if ping -c 1 -W 1 $ip>/dev/null
+ then
+  echo "$ip - $host - OnLine"
+  mysql --host=$MySQLS --user=$MySQLU --password=$MySQLP -D $MySQLD \
+	-Bse "INSERT INTO $MySQLT (hostname,ip,fecha) \
+	VALUES ('$host','$ip','$fecha2');"
+ else
+  echo "$ip - $host - OffLine"
+ fi
+
+I=`expr $I + 1`
+done
+
